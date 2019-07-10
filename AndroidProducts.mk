AmulyaX/android_device_xiaomi_lavender

jasmine: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
Bug: 77599627
Test: lunch  [see entries in list]
Change-Id: I7f06edb331a2a56f29db26294b2ece28666d2103
Signed-off-by: PIPIPIG233666 <2212848813@qq.com>

AndroidProducts.mk
@@ -24,3 +24,8 @@
 # inherit MI A2 HAVOC device config
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/havoc_jasmine_sprout.mk
+
+COMMON_LUNCH_CHOICES := \
+    havoc_jasmine_sprout-eng \
+    havoc_jasmine_sprout-userdebug \
+		havoc_jasmine_sprout-eng

AICP/device_xiaomi_jasmine_sprout

AndroidProducts.mk
@@ -24,3 +24,9 @@
 # inherit MI A2 AICP device config
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aicp_jasmine_sprout.mk
+
+COMMON_LUNCH_CHOICES := \
+    aicp_jasmine_sprout-eng \
+    aicp_jasmine_sprout-userdebug \
+		aicp_jasmine_sprout-eng
+